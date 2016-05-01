24b63aef4d082bd751f27d59a8cf2110d81417a9
Niels van Galen Last <nvangalenlast@gmail.com>
2016-05-01 16:48:13 -0700

    added various endpoints

--- app.js
+++ app.js
@@ -23,17 +23,127 @@ server.route({
     }
 });
 
+server.route({
+    method: "GET",
+    path: "/users",
+    handler: function (request, reply) {
+        models.User.findAll().then(function(users) {
+            return reply(users.map(function (user) {
+                return user.toJSON();
+            }));
+        })
+    }
+});
+
+server.route({
+    method: "GET",
+    path: "/users/increment_age",
+    handler: function (request, reply) {
+        console.log(request.query);
+        models.User.findById(request.query.user_id).then(function(user) {
+            if (user) {
+                user.update({
+                    age: user.age + 1,
+                    first_name: request.query.first_name
+                }).then(function (user) {
+                    return reply(user);
+                });
+            }
+        });
+    }
+});
+
+server.route({
+    method: "GET",
+    path: "/users/revisions",
+    handler: function (request, reply) {
+        console.log(request.query);
+        models.User.findById(request.query.user_id, {
+            include: [{model: models.Revision, include: [models.RevisionChange]}]
+        }).then(function(user) {
+            return reply(user);
+        });
+    }
+});
+
+server.route({
+    method: "GET",
+    path: "/revisions",
+    handler: function (request, reply) {
+        models.Revision.findAll().then(function(revisions) {
+            return reply(revisions.map(function (revision) {
+                return revision.toJSON();
+            }));
+        });
+    }
+});
+
+server.route({
+    method: "GET",
+    path: "/revisions/users",
+    handler: function (request, reply) {
+        console.log(request.query);
+        models.Revision.findAll({
+            where: {
+                model: 'User'
+            }
+        }).then(function(revisions) {
+            return reply(revisions.map(function (revision) {
+                return revision.toJSON();
+            }));
+        });
+    }
+});
+
+server.route({
+    method: "GET",
+    path: "/revisions/users/first_name",
+    handler: function (request, reply) {
+        console.log(request.query);
+        models.Revision.findAll({
+            where: {
+                model: 'User'
+            },
+            include: [{
+                model: models.RevisionChange,
+                where: {
+                    path: 'first_name'
+                }
+            }]
+        }).then(function(revisions) {
+            return reply(revisions.map(function (revision) {
+                return revision.toJSON();
+            }));
+        });
+    }
+});
+
+server.route({
+    method: 'GET',
+    path: '/test',
+    handler: function(request, reply) {
+        console.log(models.User.associations)
+        console.log(models.Post.associations)
+        console.log(models.Revision.associations)
+        console.log(models.RevisionChange.associations)
+        return reply({})
+    }
+});
+
 // Start the server
 server.start((err) => {
     if (err) {
         throw err;
     }
 
+    var seed = true;
+    // models.User.findAll().then(function(users) {
+    //     seed = users.length ? false : true;
+    // });
+
     // sync sequelize db
     models.sequelize.sync({force: seed})
     .then(function () {
-        // console.log(something)
-
         if (seed) {
             if (debug) {
                 console.log("perform db seeding");
