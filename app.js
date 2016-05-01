990ca1d16d4e3a2445a3e0435dcd82b404c4b944
Niels van Galen Last <nvangalenlast@gmail.com>
2016-04-30 23:36:32 -0700

    added eslint

--- app.js
+++ app.js
@@ -1,44 +1,50 @@
-'use strict';
+"use strict";
 
-const Hapi = require('hapi');
-var models =  require('./models');
-var sequelize_fixtures = require('sequelize-fixtures');
+const Hapi = require("hapi");
+var models = require("./models");
+var sequelize_fixtures = require("sequelize-fixtures");
 
 var debug = false;
-var seed = true;
+var seed = false;
 
 // Create a server with a host and port
 const server = new Hapi.Server();
 server.connection({
-    host: 'localhost',
+    host: "localhost",
     port: 8000
 });
 
 // Add the route
 server.route({
-    method: 'GET',
-    path:'/',
+    method: "GET",
+    path: "/",
     handler: function (request, reply) {
-        return reply('Hello,! World!');
+        return reply("Hello, World!");
     }
 });
 
 // Start the server
 server.start((err) => {
-    if (err) { throw err; }
+    if (err) {
+        throw err;
+    }
 
     // sync sequelize db
     models.sequelize.sync({force: seed})
-    .then(function() {
+    .then(function () {
         // console.log(something)
 
-        if(seed) {
-            if(debug) { console.log('perform db seeding'); }
-            sequelize_fixtures.loadFile('./fixtures/users.js', models).then(function(){
-                if(debug) { console.log('done with seeding'); }
+        if (seed) {
+            if (debug) {
+                console.log("perform db seeding");
+            }
+            sequelize_fixtures.loadFile("./fixtures/users.js", models).then(function () {
+                if (debug) {
+                    console.log("done with seeding");
+                }
             });
         }
 
-        console.log('Server running at:', server.info.uri);
+        console.log("Server running at:", server.info.uri);
     });
 });
