8b0de73d512ccd64f055e3aad77074e25012f245
Niels van Galen Last <nvangalenlast@gmail.com>
2016-04-30 22:42:07 -0700

    load fixtures

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 'use strict';
 
 const Hapi = require('hapi');
+var models =  require('./models');
+var sequelize_fixtures = require('sequelize-fixtures');
 
 // Create a server with a host and port
 const server = new Hapi.Server();
@@ -14,14 +16,31 @@ server.route({
     method: 'GET',
     path:'/',
     handler: function (request, reply) {
-        return reply('Hello, World!');
+        return reply('Hello,! World!');
     }
 });
+
+var seed = true;
 
 // Start the server
 server.start((err) => {
     if (err) {
         throw err;
     }
-    console.log('Server running at:', server.info.uri);
+
+    // sync sequelize db
+    models.sequelize.sync({force: seed})
+    .then(function() {
+        // console.log(something)
+
+        if(seed) {
+            console.log('perform db seeding')
+            sequelize_fixtures.loadFile('./fixtures/users.js', models).then(function(){
+                console.log('done with seeding');
+            });
+        }
+
+        console.log('Server running at:', server.info.uri);
+    });
+
 });
